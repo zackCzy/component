c05e38f3797498d6d1222e032f91aea60ec61164
zack <496928424@qq.com>
2015-01-02 00:11:03 +0800

    classify

---
+++ rollchartJs/main.js
@@ -0,0 +1,22 @@
+require.config({
+	paths:{
+		jquery:"../baseJS/jquery-1.11.0"
+	}
+});
+
+require(['jquery','rollchart'],function($,r){
+	$(function(){
+		var img1=new Image();
+	  	img1.src="img/img1.jpg";
+	  	var img2=new Image();
+	  	img2.src="img/img3.jpg";
+		var rollchart=new r.Rollchart();
+		rollchart.init({
+			image:[img1,img2],
+			height:300,
+			alts:["环球高富1","环球高富2"],
+			phone:true
+		},"body");
+		rollchart.setWidth(600);
+	});
+});
